acbcf53bb1c1feb55aff62e6ed66b186c003c4ef
Komal Sharma <sh.komal1994@gmail.com>
2020-02-12 13:29:57 +0000

    Changes done in DisplayTablePatternA and DisplayTablePatternB

--- comp30830/src/practical3/DisplayTablePatternB.java
+++ comp30830/src/practical3/DisplayTablePatternB.java
@@ -2,23 +2,23 @@ package practical3;
 
 import java.util.Scanner;
 
-public class DisplayTablePatternA {
+public class DisplayTablePatternB {
 
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Enter the integer: ");
-		
+
+		System.out.println("Enter the integer: ");
+
 		int integer = input.nextInt();
-		
+
 		for (int i=1;i<=integer;i++) {
-			for (int j=1;j<=i;j++)
+			for (int j=1;j<=integer+1-i;j++)
 				System.out.print(j + " ");
 			System.out.println();
-			
 		}
-
+		
+		//close the scanner
+		input.close();
 	}
-
 }
